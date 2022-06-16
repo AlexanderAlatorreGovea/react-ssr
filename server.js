91a5371450203fa692353329eabc6aa822ebf5c0
Alexander <Alexander_alatorre1993@hotmail.com>
2022-06-15 20:41:49 -0600

    added router

--- server.js
+++ server.js
@@ -1,7 +1,23 @@
-const express = require("express");
+import "babel-polyfill";
+import express from "express";
+import React from "react";
+import ReactDomServer from "react-dom/server";
+import { StaticRouter } from "react-router";
+import App from "./src/App";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+app.get("*", (req, res) => {
+  const container = ReactDomServer.renderToString(
+    <StaticRouter
+        
+    >
+      <App />
+    </StaticRouter>
+  );
+  res.send("this is my working app");
+});
 
 app.listen(PORT, () => {
   console.log(`App running on ${PORT}`);
--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,35 @@
+const path = require("path");
+const webpackNodeExternals = require("webpack-node-externals");
+
 module.exports = {
-  entry: "./src/index.js",
+  target: "node",
+  entry: "./server.js",
   module: {
     loaders: [
       { test: /\.js$/, loader: "babel-loader", exclude: /node_modules/ },
       { test: /\.jsx$/, loader: "babel-loader", exclude: /node_modules/ },
     ],
+    rules: [
+      {
+        options: {
+          presets: [
+            "react",
+            "stage-0",
+            [
+              "env",
+              {
+                target: { browsers: ["last 2 versions"] },
+              },
+            ],
+          ],
+        },
+      },
+    ],
   },
   output: {
     filename: "bundle.js",
-    path: __dirname + "/public",
+    path: path.resolve(__dirname + "build"),
+    publicPath: "/build",
   },
+  externals: [webpackNodeExternals()],
 };
